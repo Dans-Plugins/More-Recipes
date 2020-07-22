57d5a56befe32b670e74016d70e4adbb6ef5c4dd
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-21 23:03:18 -0600

    implemented Salt.java

--- src/main/java/recipesystem/ItemStacks/Salt.java
+++ src/main/java/recipesystem/ItemStacks/Salt.java
@@ -1,7 +1,28 @@
 package recipesystem.ItemStacks;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Salt {
 
+    public static ItemStack getItemStack(int amount) {
+        ItemStack item = new ItemStack(Material.SUGAR, amount);
+        ItemMeta meta = item.getItemMeta();
 
+        meta.setDisplayName(ChatColor.WHITE + "Salt");
+        List<String> lore = new ArrayList<String>();
+        lore.add("");
+        lore.add(ChatColor.WHITE + "" + ChatColor.ITALIC + "Great for seasoning.");
+
+        meta.setLore(lore);
+        item.setItemMeta(meta);
+
+        return item;
+    }
 
 }
--- src/main/java/recipesystem/Subsystems/ItemStackSubsystem.java
+++ src/main/java/recipesystem/Subsystems/ItemStackSubsystem.java
@@ -1,7 +1,16 @@
 package recipesystem.Subsystems;
 
+import org.bukkit.inventory.ItemStack;
+import recipesystem.ItemStacks.Salt;
+
 public class ItemStackSubsystem {
 
+    public ItemStack getItemStack(String itemName, int amount) {
 
+        if (itemName.equalsIgnoreCase("Salt")) {
+            return Salt.getItemStack(amount);
+        }
+        return null;
+    }
 
 }
