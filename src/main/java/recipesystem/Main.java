3ffae2b1eab986e3d84430cf0f4783fcfc0e53c0
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-21 22:36:02 -0600

    initial commit

---
+++ src/main/java/recipesystem/Main.java
@@ -0,0 +1,16 @@
+package recipesystem;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
