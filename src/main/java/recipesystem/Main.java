5e979943a2332c016bd3142df3ff115adf605936
dmccoystephenson <dmccoystephenson@gmail.com>
2020-07-21 23:09:39 -0600

    added ItemStackSubsystem to Main.java

--- src/main/java/recipesystem/Main.java
+++ src/main/java/recipesystem/Main.java
@@ -1,8 +1,12 @@
 package recipesystem;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import recipesystem.Subsystems.ItemStackSubsystem;
 
 public final class Main extends JavaPlugin {
+
+    // subsystems
+    ItemStackSubsystem itemstacks = new ItemStackSubsystem(this);
 
     @Override
     public void onEnable() {
--- src/main/java/recipesystem/Subsystems/ItemStackSubsystem.java
+++ src/main/java/recipesystem/Subsystems/ItemStackSubsystem.java
@@ -2,8 +2,15 @@ package recipesystem.Subsystems;
 
 import org.bukkit.inventory.ItemStack;
 import recipesystem.ItemStacks.Salt;
+import recipesystem.Main;
 
 public class ItemStackSubsystem {
+
+    Main main = null;
+
+    public ItemStackSubsystem(Main plugin) {
+        main = plugin;
+    }
 
     public ItemStack getItemStack(String itemName, int amount) {
 
